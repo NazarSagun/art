f3a569e8f9a078dcbe7cf99a8e7897457a880c08
NazarSagun <nazar.sagun@gmail.com>
2020-08-18 13:34:28 +0100

    copmleted

--- script.js
+++ script.js
@@ -4785,20 +4785,18 @@ __webpack_require__.r(__webpack_exports__);
 
 
 var mask = function mask(selector) {
-  var setCursorPosition = function setCursorPosition(pos, elem) {
-    elem.focus();
-
-    if (elem.setSelectionRange) {
-      elem.setSelectionRange(pos, pos);
-    } else if (elem.createTextRange) {
-      var range = elem.createTextRange();
-      range.collapse(true);
-      range.moveEnd('character', pos);
-      range.moveStart('character', pos);
-      range.select();
-    }
-  };
-
+  // let setCursorPosition = (pos, elem) => {
+  //     elem.focus();
+  //     if (elem.setSelectionRange) {
+  //         elem.setSelectionRange(pos, pos);
+  //     } else if (elem.createTextRange) {
+  //         let range = elem.createTextRange();
+  //         range.collapse(true);
+  //         range.moveEnd('character', pos);
+  //         range.moveStart('character', pos);
+  //         range.select();
+  //     }
+  // };
   function createMask(event) {
     var matrix = '+351 (___) ___ ___',
         i = 0,
@@ -4811,15 +4809,13 @@ var mask = function mask(selector) {
 
     this.value = matrix.replace(/./g, function (a) {
       return /[_\d]/.test(a) && i < val.length ? val.charAt(i++) : i >= val.length ? '' : a;
-    });
-
-    if (event.type === 'blur') {
-      if (this.value.length == 2) {
-        this.value = '';
-      } else {
-        setCursorPosition(this.value.length, this);
-      }
-    }
+    }); // if (event.type === 'blur') {
+    //     if (this.value.length == 2) {
+    //         this.value = '';
+    //     } else {
+    //         setCursorPosition(this.value.length, this);
+    //     }
+    // }
   }
 
   var inputs = document.querySelectorAll(selector);
